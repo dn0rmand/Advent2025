84dfb4d4466452f98410f244ab6ff88179e7e41a
Dominique Normand <dominique@normand.us>
2025-12-06 09:49:01 -0500

    day 5 done

--- day5.ts
+++ day5.ts
@@ -1,6 +1,15 @@
 import { Day } from './tools/day.ts'
 
-type TInput = string[]
+type TRange = {
+  from: number
+  to: number
+}
+
+type TInput = {
+  ranges: TRange[]
+  ingredients: number[]
+}
+
 export class Day5 extends Day<TInput> {
   constructor() {
     super(5)
@@ -8,15 +17,59 @@ export class Day5 extends Day<TInput> {
 
   loadInput(): TInput {
     const data = this.readDataFile()
-    return data
+    const ranges: TRange[] = []
+    const ingredients: number[] = []
+
+    let index = 0
+    // Load ranges
+    while (index < data.length) {
+      const line = data[index++]
+      if (line.length === 0) {
+        break
+      }
+      const [from, to] = line.split('-').map(v => +v)
+      ranges.push({ from, to })
+    }
+
+    ranges.sort((a, b) => {
+      const d = a.from - b.from
+      return d ? d : a.to - b.to
+    })
+
+    // Load ingredients
+    while (index < data.length) {
+      ingredients.push(+data[index++])
+    }
+
+    return { ranges, ingredients }
   }
 
-  part1(_: TInput): number {
-    return 0
+  part1(input: TInput): number {
+    const good = input.ingredients.filter(ingredient => input.ranges.some(({ from, to }) => from <= ingredient && ingredient <= to))
+
+    return good.length
   }
 
-  part2(_: TInput): number {
-    return 0
+  part2(input: TInput): number {
+    let lastTo = 0
+    let total = 0
+
+    for (let i = 0; i < input.ranges.length; i++) {
+      let { from, to } = input.ranges[i]
+
+      if (lastTo >= to) {
+        continue
+      }
+
+      if (from <= lastTo) {
+        from = lastTo + 1
+      }
+
+      total += to - from + 1
+      lastTo = to
+    }
+
+    return total
   }
 }
 
