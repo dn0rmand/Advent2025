f010bebee963a0a8200559f1d7c27ecae7ddf5bc
Dominique Normand <dominique@normand.us>
2025-12-12 16:26:05 -0500

    day 9 part 1 done

--- day9.ts
+++ day9.ts
@@ -1,6 +1,11 @@
 import { Day } from './tools/day.ts'
 
-type TInput = string[]
+type TPoint = {
+  x: number
+  y: number
+}
+type TInput = TPoint[]
+
 export class Day9 extends Day<TInput> {
   constructor() {
     super(9)
@@ -8,11 +13,46 @@ export class Day9 extends Day<TInput> {
 
   loadInput(): TInput {
     const data = this.readDataFile()
-    return data
+    const xConnect: Map<number, TPoint[]> = new Map()
+    const yConnect: Map<number, TPoint[]> = new Map()
+
+    const add = (m: Map<number, TPoint[]>, k: number, pt: TPoint) => {
+      let a = m.get(k)
+      if (a === undefined) {
+        a = []
+        m.set(k, a)
+      }
+      a.push(pt)
+      if (a.length > 2) {
+        throw 'Error'
+      }
+    }
+
+    const input = data.map(line => {
+      const [x, y] = line.split(',').map(v => +v)
+      const pt: TPoint = { x, y }
+      const ax = xConnect.get(x)
+
+      add(xConnect, x, pt)
+      add(yConnect, y, pt)
+      return { x, y }
+    })
+    return input
   }
 
-  part1(_: TInput): number {
-    return 0
+  part1(input: TInput): number {
+    let maxArea = 0
+    for (let i = 1; i < input.length; i++) {
+      const p1 = input[i]
+      for (let j = 0; j < i; j++) {
+        const p2 = input[j]
+        const area = (Math.abs(p1.x - p2.x) + 1) * (Math.abs(p1.y - p2.y) + 1)
+        if (area > maxArea) {
+          maxArea = area
+        }
+      }
+    }
+    return maxArea
   }
 
   part2(_: TInput): number {
