61060d08491e02ccce736cf80bdc38172c60d7d2
dn0rmand <dominique@normand.us>
2025-12-21 16:50:24 -0500

    day 9 done

--- day9.ts
+++ day9.ts
@@ -1,10 +1,11 @@
 import { Day } from './tools/day.ts'
+import { TPoint, Polygon } from './tools/geometric/polygonInPolygon.ts'
 
-type TPoint = {
-  x: number
-  y: number
+type TInput = {
+  points: TPoint[]
+  xConnect: Map<number, TPoint[]>
+  yConnect: Map<number, TPoint[]>
 }
-type TInput = TPoint[]
 
 export class Day9 extends Day<TInput> {
   constructor() {
@@ -28,24 +29,30 @@ export class Day9 extends Day<TInput> {
       }
     }
 
-    const input = data.map(line => {
+    let maxX = 1
+    let maxY = 1
+
+    const points = data.map(line => {
       const [x, y] = line.split(',').map(v => +v)
       const pt: TPoint = { x, y }
-      const ax = xConnect.get(x)
 
+      maxX = Math.max(x, maxX)
+      maxY = Math.max(y, maxY)
       add(xConnect, x, pt)
       add(yConnect, y, pt)
       return { x, y }
     })
-    return input
+
+    return { points, xConnect, yConnect }
   }
 
   part1(input: TInput): number {
     let maxArea = 0
-    for (let i = 1; i < input.length; i++) {
-      const p1 = input[i]
+    const points = input.points
+    for (let i = 1; i < points.length; i++) {
+      const p1 = points[i]
       for (let j = 0; j < i; j++) {
-        const p2 = input[j]
+        const p2 = points[j]
         const area = (Math.abs(p1.x - p2.x) + 1) * (Math.abs(p1.y - p2.y) + 1)
         if (area > maxArea) {
           maxArea = area
@@ -55,8 +62,44 @@ export class Day9 extends Day<TInput> {
     return maxArea
   }
 
-  part2(_: TInput): number {
-    return 0
+  isValid(p1: TPoint, p2: TPoint, polygon: Polygon): boolean {
+    const minX = Math.min(p1.x, p2.x)
+    const maxX = Math.max(p1.x, p2.x)
+    const minY = Math.min(p1.y, p2.y)
+    const maxY = Math.max(p1.y, p2.y)
+
+    const innerPolygon = [
+      { x: minX + 1, y: minY + 1 },
+      { x: maxX - 1, y: minY + 1 },
+      { x: maxX - 1, y: maxY - 1 },
+      { x: minX + 1, y: maxY - 1 },
+      { x: minX + 1, y: minY + 1 },
+    ]
+
+    const res = polygon.containsPolygon(innerPolygon)
+
+    return res
+  }
+
+  part2(input: TInput): number {
+    const polygon = new Polygon(input.points, input.xConnect, input.yConnect)
+
+    let maxArea = 0
+    const points = input.points
+    for (let i = 1; i < points.length; i++) {
+      const p1 = points[i]
+      for (let j = 0; j < i; j++) {
+        const p2 = points[j]
+        const area = (Math.abs(p1.x - p2.x) + 1) * (Math.abs(p1.y - p2.y) + 1)
+        if (area <= maxArea || !this.isValid(p1, p2, polygon)) {
+          continue
+        }
+        if (area > maxArea) {
+          maxArea = area
+        }
+      }
+    }
+    return maxArea
   }
 }
 
